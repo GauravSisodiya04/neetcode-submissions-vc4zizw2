2d805f2a1d42ffd24077ed3385f00d630c97563e
GauravSisodiya04 <btech.gsdn@gmail.com>
2026-04-09 08:37:06 -0600

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+    //We will use Floyd's cycle detection algorithm
+    //Tortoise and Hare method
+    if(head === null || head.next ===null){
+        return false;
+    }
+    let fast = head;
+    let slow = head;
+
+    while(fast !== null && fast.next !== null){
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if(slow === fast){
+            return true;
+        }
+    }
+
+    return false;
+    }
+}
