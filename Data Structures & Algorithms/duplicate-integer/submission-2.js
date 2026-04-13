2cd36c00503de0702e0a0db99b300111e5a97a00
GauravSisodiya04 <btech.gsdn@gmail.com>
2026-04-13 07:22:11 -0600

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+       // Create a HashSet
+
+       let visited = new Set();
+
+       for(let eachNum of nums){
+       if(visited.has(eachNum)){
+        return true;
+       } else {
+        visited.add(eachNum)
+       }
+       
+       }
+
+       return false;
+
+    }
+}
