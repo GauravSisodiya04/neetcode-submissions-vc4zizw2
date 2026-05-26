a2daa11384bfccc507e166659bf7733094db8382
GauravSisodiya04 <btech.gsdn@gmail.com>
2026-05-26 07:44:33 -0600

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+
+    // House Robber Space Optimised Version
+
+    // Lets say we are looking at the third house . Lets call the house immediate 
+    // previous to this one as prev1 and previous of immediate previous as prev2.
+
+    // In case of three house, house1 will be prev2, house2 will be prev1.
+
+    let prev2 =0; 
+    let prev1 = 0;
+
+    for(let everyHouse of nums){
+        
+        let current = Math.max(everyHouse+prev2, prev1);// 1
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return prev1;   
+    }
+}
+
